6a3c046a412a1890b9d44b6dac4109dae9f29fe6
Max <maxwell.aeden@gmail.com>
2023-06-14 16:32:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,43 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from time import sleep
-
-file_ids = "user_ids.txt"
-file_out = "out.txt"
-sep = "|"  # This is how values will be separated in the exported file. Commas aren't preferred, because song titles
-# often include commas.
-
-# with indexing, real names, ML usernames, and ML user IDs are associated with each other
-# Real name = the name that will appear in the header of the exported file - your choice
-# ML username = the person's display name on musicleague.com
-# ML user ID = the unique string associated with each user's profile on musicleague.com,
-# which follows /user/ in their profile's URL.
-# Make a file user_ids.txt with each line representing one person in the league in the format realname=username=userid
-# Example: Max=max=ef5170c99dbd4c2e9224b9fdb1780a35
-# Creating this automatically will be added in a future update.
-# TODO: automate this part
-id_list = ''
-try:
-	id_list = open(file_ids, "r", encoding='utf8')
-except FileNotFoundError:
-	print("ERROR: user_ids.txt doesn't exist. Please create a file user_ids.txt in the same directory as main.py,"
-	      "and add people to it in the format RealName=MLusername=MLuserID for each line."
-	      "(e.g. Max=max=ef5170c99dbd4c2e9224b9fdb1780a35)"
-	      "This will be automated in a future update.")
-	sleep(5)
-	quit()
-real_names = []
-usernames = []
-user_ids = []
-for line in id_list:
-	name = line.split("=")[0]
-	username = line.split("=")[1]
-	id = line.split("=")[2].rstrip()
-	real_names.append(name)
-	usernames.append(username)
-	user_ids.append(id)
-print(id_list)
-id_list.close()
+from file_options import user_ids, real_names, usernames, sep, file_out
 
 
 def get_name_from_id(this_id):
@@ -159,7 +123,7 @@ def parse_song_info(driver, i):  # returns data for 1 song and its votes and for
 
 
 def add_bells(song_line, week, url):  # some extra automation for quicker transfer to Excel, this can be modified
-	song_string = "TRIM(CLEAN(HYPERLINK(CONCAT($D2),$E2)))"
+	song_string = "=TRIM(CLEAN(HYPERLINK(CONCAT($D2),$E2)))"
 	avg = "Z2/AA2"
 	week_url = ("=HYPERLINK(\"" + url + "\",{})".format(week))
 	song_line2 = week_url + sep + song_string + sep + song_line + sep + avg
@@ -177,8 +141,6 @@ def create_header():  # the top row of the exported file, should only be run onc
 
 def main():
 	print("Type quit to exit.")
-	# create_header()
-	# TODO: check if we need header or not
 	url = input("url: ")
 	week = input("round: ")
 	if url == "quit" or week == "quit":
